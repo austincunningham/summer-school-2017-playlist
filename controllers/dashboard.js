2a8f3a19de8ff6e89a3e81213aba22fe08c2bbea
System <none>
2017-06-13 19:54:37 +0000

    🏎🌚 version 2 of playlist app

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,17 +2,24 @@
 
 const logger = require('../utils/logger');
 const playlistCollection = require('../models/playlist-store.js');
+const playlistStore = require('../models/playlist-store');
 
 const dashboard = {
   index(request, response) {
     logger.info('dashboard rendering');
     const viewData = {
       title: 'Playlist Dashboard',
-      playlists: playlistCollection,
+      playlists: playlistStore.getAllPlaylists(),
     };
-    logger.info('about to render', playlistCollection);
+    logger.info('about to render', playlistStore.getAllPlaylists());
     response.render('dashboard', viewData);
   },
+  deleteplaylist(request, response) {
+    console.log("ID of playlist to be deleted : "+ request.params.id);
+    playlistStore.removePlaylist(request.params.id);
+    response.redirect('/dashboard');
+  }
 };
+
 
 module.exports = dashboard;
--- routes.js
+++ routes.js
@@ -6,9 +6,14 @@ const router = express.Router();
 const dashboard = require('./controllers/dashboard.js');
 const about = require('./controllers/about.js');
 const welcome = require('./controllers/welcome.js');
+const playlist = require('./controllers/playlist.js');
 
 router.get('/', welcome.index);
 router.get('/dashboard', dashboard.index);
 router.get('/about', about.index);
+router.get('/playlist/:id', playlist.index);
+router.get('/playlist/:id/deletesong/:songid', playlist.deleteSong);
+router.get('/dashboard/deleteplaylist/:id', dashboard.deleteplaylist);
+
 
 module.exports = router;
